acd4ca9abc7b1241d9290ea415ac4c8fa485b1d4
GROMIT13 <przem9.pp@gmail.com>
2023-03-16 18:44:33 +0100

    Added Construcors/Destructor

---
+++ Engine/DataStructures/Rect.cpp
@@ -0,0 +1,29 @@
+#include "Rect.hpp"
+#include "vec.hpp"
+
+//TO DO: Check if it really works
+
+Rect::Rect(int x, int y, int width, int height)
+{
+	position = new Vec2(x,y);
+	dimension = new Vec2(width, height);
+	buffer = new CHAR_INFO[width * height];
+	memset(buffer, 0, sizeof(CHAR_INFO) * width * height);
+}
+
+Rect::Rect(const Rect& other)
+{
+	position = new Vec2(other.position->x, other.position->y);
+	dimension = new Vec2(other.dimension->x, other.dimension->y);
+	buffer = new CHAR_INFO[other.dimension->x * other.dimension->y];
+	memcpy(buffer, other.buffer, sizeof(CHAR_INFO) * other.dimension->x * other.dimension->y);
+}
+
+Rect::~Rect()
+{
+	delete position;
+	delete dimension;
+	delete[] buffer;
+}
+
+
--- Engine/DataStructures/Rect.hpp
+++ Engine/DataStructures/Rect.hpp
@@ -1,1 +1,17 @@
 #pragma once
+#include <Windows.h>
+
+class Vec2;
+
+class Rect
+{
+private:
+	Vec2* position;
+	Vec2* dimension;
+	CHAR_INFO* buffer;
+
+public:
+	Rect(int x,int y,int width,int height);
+	Rect(const Rect& other);
+	~Rect();
+};
