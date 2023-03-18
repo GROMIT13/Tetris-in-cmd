b9e34151fcf1d14faa3664bac8d7c0fa80d62874
GROMIT13 <przem9.pp@gmail.com>
2023-03-18 23:48:56 +0100

    Made tests with log class

--- Main.cpp
+++ Main.cpp
@@ -5,8 +5,16 @@
 int main()
 {
 	Log log("log.txt");
-	log.Info("test Info");
-	log.Warn("test Warn");
-	log.Error("test Error");
+	Log log2("log.txt");
+	log.ClearFile();
+	log.Info("1");
+	log2.Info("2");
+	log.Warn("3");
+	log.Warn("4");
+	log.Error("5");
+	log2.Error("6");
+	
+	
+	
 	return 0;
 }
