0b11976a32251fa6b2d36ec23acae2cef44d370d
GROMIT13 <przem9.pp@gmail.com>
2023-03-19 23:17:24 +0100

    Made tests

--- Main.cpp
+++ Main.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include "Engine/DataStructures/vec.hpp"
+#include "Engine/DataStructures/Rect.hpp"
 #include "Log.hpp"
+
+#define LOG(x) std::cout << x << std::endl;
 
 int main()
 {
-	Log log("log.txt");
-	Log log2("log.txt");
-	log.ClearFile();
-	log.Info("1");
-	log2.Info("2");
-	log.Warn("3");
-	log.Warn("4");
-	log.Error("5");
-	log2.Error("6");
-	
-	
-	
 	return 0;
 }
