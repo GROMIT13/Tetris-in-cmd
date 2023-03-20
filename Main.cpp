f40df3a266cd5db6480beb42c0b735fa5b00eb46
GROMIT13 <przem9.pp@gmail.com>
2023-03-20 23:55:51 +0100

    Made test with GConsole

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,19 @@
 #include "Engine/DataStructures/vec.hpp"
 #include "Engine/DataStructures/Rect.hpp"
 #include "Log.hpp"
+#include "Engine/GConsole.hpp"
 
 #define LOG(x) std::cout << x << std::endl;
 
 int main()
 {
+	bool windowRunning = true;
+	GConsole screen(40, 20, 4, 4);
+
+	while (windowRunning)
+	{
+		screen.Draw(0, 0);
+		screen.Update();
+	}
 	return 0;
 }
