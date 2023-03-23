bb62a4ad20db29cac7b03775927019a671ee7d34
GROMIT13 <przem9.pp@gmail.com>
2023-03-23 21:38:10 +0100

    added basic input system

--- Engine/Input.cpp
+++ Engine/Input.cpp
@@ -1,1 +1,35 @@
+#include "Input.hpp"
+#include <Windows.h>
 
+Input::Input()
+{
+	keyState = new KeyState[256];
+	memset(keyState, 0, sizeof(KeyState) * 256);
+}
+
+Input::~Input()
+{
+	delete[] keyState;
+}
+
+void Input::CheckInput()
+{
+	for (int i = 0; i < 256; i++)
+	{
+		if (keyState[i].present)
+			keyState[i].past = true;
+		else
+			keyState[i].past = false;
+
+
+		if (GetAsyncKeyState(i))
+			keyState[i].present = true;
+		else
+			keyState[i].present = false;
+	}
+}
+
+KeyState Input::GetKey(char keyCode)
+{
+	return keyState[keyCode];
+}
--- Engine/Input.hpp
+++ Engine/Input.hpp
@@ -1,1 +1,24 @@
 #pragma once
+
+#include <vector>
+
+struct KeyState
+{
+	bool present;
+	bool past;
+};
+
+
+//TO DO: Add more input states
+//		 Move Input to singielton class
+class Input
+{
+private:
+	KeyState* keyState;
+
+public:
+	Input();
+	~Input();
+	void CheckInput();
+	KeyState GetKey(char keyCode);
+};
