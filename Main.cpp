bb4c3be98e7b8549bc4663ff2da86bce9d403557
GROMIT13 <przem9.pp@gmail.com>
2023-03-28 23:49:41 +0200

    new rect function test

--- Main.cpp
+++ Main.cpp
@@ -3,18 +3,35 @@
 #include "Engine/DataStructures/Rect.hpp"
 #include "Log.hpp"
 #include "Engine/GConsole.hpp"
+#include "Engine/Input.hpp"
 
 #define LOG(x) std::cout << x << std::endl;
 
 int main()
 {
-	bool windowRunning = true;
-	GConsole screen(40, 20, 4, 4);
+	GConsole screen("Tetris", 60, 40, 16, 16);
+	Input::SetKeys({ VK_SPACE,VK_UP,VK_DOWN,VK_LEFT,VK_RIGHT,VK_ESCAPE });
+	Rect rect(0, 0, 4, 6);
+	Vec2 position(0, 0);
+	rect.Fill('P',FG_COLOR_LIGHT_YELLOW);
 
-	while (windowRunning)
+	while (!Input::GetKey(VK_ESCAPE).present)
 	{
-		screen.Draw(0, 0);
-		screen.Update();
+		if (Input::GetState(VK_UP) == State::Enter)
+			position.y--;
+		if (Input::GetState(VK_DOWN) == State::Enter)
+			position.y++;
+		if (Input::GetState(VK_LEFT) == State::Enter)
+			position.x--;
+		if (Input::GetState(VK_RIGHT) == State::Enter)
+			position.x++;
+		rect.SetPosition(position);
+
+		screen.DrawRect(rect);
+		//screen.DrawRect(rect.GetPosition().x, rect.GetPosition().y, rect.GetBuffer(), rect.GetDimension().x, rect.GetDimension().y);
+		screen.UpdateScreen();
+		screen.ClearBuffer();
+		Input::CheckInput();
 	}
 	return 0;
 }
