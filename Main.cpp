54ed9a82ce83c48e8eb18ecfc82474ad9b9221c3
GROMIT13 <przem9.pp@gmail.com>
2023-04-17 20:30:26 +0200

    init commit

--- Main.cpp
+++ Main.cpp
@@ -13,54 +13,59 @@ Tetromino::Type debugChangeBlock();
 int main()
 {
 	GConsole screen("Tetris", 60, 40, 16, 16);
-	Input::SetKeys({ VK_SPACE,VK_UP,VK_DOWN,VK_LEFT,VK_RIGHT,VK_ESCAPE,VK_RETURN,'Q','W','E','R','T','Y','U','Z','X','A','P'});
+	Input::SetKeys({ VK_SPACE,VK_UP,VK_DOWN,VK_LEFT,VK_RIGHT,VK_ESCAPE,VK_RETURN,'1','2','3','4','5','6','7','Z','X','A','C','P','R'});
 	Vec2 position(5, 5);
 	Clock clock(100);
-	Tetromino tetromino(5, 5, Tetromino::Type::I_BLOCK);
 	Board board(10,10);
+	Tetromino tetromino(5, 5, 800, board);
+	int count = 0;
 
 	while (!Input::GetKey(VK_ESCAPE).present)
 	{
 		tetromino.ChangeBlock(debugChangeBlock());
 		if (Input::GetState('Z') == State::Enter)
+			tetromino.Rotate(3);
+		if (Input::GetState('X') == State::Enter)
 			tetromino.Rotate(1);
 		if (Input::GetState('A') == State::Enter)
 			tetromino.Rotate(2);
-		if (Input::GetState('X') == State::Enter)
-			tetromino.Rotate(3);
-		if (Input::GetState(VK_RETURN) == State::Enter)
-			board.MoveNextList();
 		if (Input::GetState('P') == State::Enter)
 			board.PlaceBlock(tetromino);
 
 		if (Input::GetState(VK_UP) == State::Enter)
-			tetromino.Move( 0, -1);
-		if (Input::GetState(VK_DOWN) == State::Enter)
-			tetromino.Move( 0,  1);
-		if (Input::GetState(VK_LEFT) == State::Enter)
-			tetromino.Move(-1, 0);
-		if (Input::GetState(VK_RIGHT) == State::Enter)
-			tetromino.Move( 1, 0);
+			tetromino.Move(0, -1);
 		
+		if (tetromino.DoesFit(tetromino,tetromino.GetPosition().x, tetromino.GetPosition().y + 1))
+			screen.Draw(0, 0, CHARACTER_FULL, FG_COLOR_GREEN);
+		else
+			screen.Draw(0, 0, CHARACTER_FULL, FG_COLOR_RED);
+
+		// LOGIC ----------------------------------------------------------------------------------
+		tetromino.Update();
+		board.Update(tetromino);
+		
+		// DRAWING --------------------------------------------------------------------------------
 		board.DrawBoard(screen, tetromino);
-		screen.DrawRectTransparent(tetromino,FG_COLOR_BLACK);
+		tetromino.DrawTetromino(screen);
+
 		screen.SetTitle("FPS: " + std::to_string(clock.GetFPS(1000)));
 		screen.UpdateScreen();
 		screen.ClearBuffer();
-		//Clock::Wait(10);
+		// INPUT ----------------------------------------------------------------------------------
 		Input::CheckInput();
 	}
+
 	return 0;
 }
 
 Tetromino::Type debugChangeBlock()
 {
-	if(Input::GetState('Q') == State::Enter) return Tetromino::Type::I_BLOCK;
-	if(Input::GetState('W') == State::Enter) return Tetromino::Type::O_BLOCK;
-	if(Input::GetState('E') == State::Enter) return Tetromino::Type::J_BLOCK;
-	if(Input::GetState('R') == State::Enter) return Tetromino::Type::L_BLOCK;
-	if(Input::GetState('T') == State::Enter) return Tetromino::Type::Z_BLOCK;
-	if(Input::GetState('Y') == State::Enter) return Tetromino::Type::S_BLOCK;
-	if(Input::GetState('U') == State::Enter) return Tetromino::Type::T_BLOCK;
+	if(Input::GetState('1') == State::Enter) return Tetromino::Type::I_BLOCK;
+	if(Input::GetState('2') == State::Enter) return Tetromino::Type::O_BLOCK;
+	if(Input::GetState('3') == State::Enter) return Tetromino::Type::J_BLOCK;
+	if(Input::GetState('4') == State::Enter) return Tetromino::Type::L_BLOCK;
+	if(Input::GetState('5') == State::Enter) return Tetromino::Type::Z_BLOCK;
+	if(Input::GetState('6') == State::Enter) return Tetromino::Type::S_BLOCK;
+	if(Input::GetState('7') == State::Enter) return Tetromino::Type::T_BLOCK;
 	return Tetromino::Type::NULL_BLOCK;
 }
